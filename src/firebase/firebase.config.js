c410b9ed009984be71a3fa5abc930f683e34ac6e
AshrafulMorsalinSizan99 <jucse28.377@gmail.com>
2022-10-26 23:58:50 +0600

    environment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,11 +6,15 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyC2SH8HUw2ftdTYnCs5f5_0G6ZM66-uOv0",
-    authDomain: "edu-hub-website.firebaseapp.com",
-    projectId: "edu-hub-website",
-    storageBucket: "edu-hub-website.appspot.com",
-    messagingSenderId: "336214503823",
-    appId: "1:336214503823:web:ba690bb09607cf8166dbfa"
+    authDomain: process.env.REACT_APP_authDomain,
+    // projectId: "edu-hub-website",
+    projectId: process.env.REACT_APP_projectId,
+    // storageBucket: "edu-hub-website.appspot.com",
+    storageBucket: process.env.REACT_APP_storageBucket,
+    // messagingSenderId: "336214503823",
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    // appId: "1:336214503823:web:ba690bb09607cf8166dbfa"
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
